e91ca5d05bc988282beea45431dd654b6dc6ef01
Kevin-Yichen Li <2358068@edu.vaniercollege.qc.ca>
2023-11-15 10:01:06 -0500

    Delete redundant statements

--- src/main/java/org.kevin/dto/RestaurantManagementSystem.java
+++ src/main/java/org.kevin/dto/RestaurantManagementSystem.java
@@ -3,9 +3,6 @@ package org.kevin.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-
-import java.util.Arrays;
 
 @EqualsAndHashCode
 @Setter
